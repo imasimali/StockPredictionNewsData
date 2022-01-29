31cfde3fab54e519513a6c5a491c31c3150937d8
Asim Ali <m.asimali69@gmail.com>
2022-01-29 10:26:57 +0000

    updates

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ import pandas as pd
 
 app = Flask(__name__)
 model = keras.models.load_model("invisor2.h5",custom_objects={'KerasLayer':hub.KerasLayer})
-text=pd.read_csv("data.csv")
-num=pd.read_csv("AAPL.csv")
+# text=pd.read_csv("data.csv")
+# num=pd.read_csv("AAPL.csv")
 
-text['combined'] = text[text.columns[1:27]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
+# text['combined'] = text[text.columns[1:27]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
 
-text['combined'].replace("[^a-zA-Z]"," ",regex=True, inplace=True)
-text['combined']=text['combined'].apply(lambda x : x.lower())
+# text['combined'].replace("[^a-zA-Z]"," ",regex=True, inplace=True)
+# text['combined']=text['combined'].apply(lambda x : x.lower())
 
-text=text['combined']
+# text=text['combined']
 
-num=num['Close']
+# num=num['Close']
 
-scaler2=MinMaxScaler(feature_range=(0,1))
-num=num.values.reshape(-1,1)
-num_s=scaler2.fit_transform(num)
+# scaler2=MinMaxScaler(feature_range=(0,1))
+# num=num.values.reshape(-1,1)
+# num_s=scaler2.fit_transform(num)
 
 @app.route('/')
 def hello_world():
@@ -30,11 +30,11 @@ def hello_world():
 @app.route("/get")
 def get_prediction():
   # stock = request.args["stock"]
-  newp=model.predict([text,num_s])
-  y_new_inverse = scaler2.inverse_transform(newp)
-  res=y_new_inverse[0][0]
+  # newp=model.predict([text,num_s])
+  # y_new_inverse = scaler2.inverse_transform(newp)
+  # res=y_new_inverse[0][0]
 
-  return 'Prediction is '.format(res)
+  return 'Prediction is '#.format(res)
 
 
 app.run(host='0.0.0.0', port=8080)
