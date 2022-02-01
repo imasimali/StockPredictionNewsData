38eed7567362f8c0bad29640efcf6f040bcecdf6
Asim Ali <hi@asimali.net>
2022-02-01 17:08:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 from tensorflow import keras
 import tensorflow_hub as hub
 import tensorflow_text as text
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+import urllib.request
+from cherrypicker import CherryPicker
 
 app = Flask(__name__)
+run_with_ngrok(app)
+CORS(app)
 
-model = keras.models.load_model("invisor2.h5",custom_objects={'KerasLayer':hub.KerasLayer})
-
-textual_file=pd.read_csv("data.csv")
-num=pd.read_csv("AAPL.csv")
-
-textual_file['combined'] = textual_file[textual_file.columns[1:27]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
-
-textual_file['combined'].replace("[^a-zA-Z]"," ",regex=True, inplace=True)
-textual_file['combined']=textual_file['combined'].apply(lambda x : x.lower())
-
-textual_file=textual_file['combined']
-num=num['Close']
-
-scaler2=MinMaxScaler(feature_range=(0,1))
-num=num.values.reshape(-1,1)
-num_s=scaler2.fit_transform(num)
-
-newp=model.predict([textual_file,num_s])
-y_new_inverse = scaler2.inverse_transform(newp)
-res=y_new_inverse[0][0]
-print("The Predicted value is : ",res)
+model = tf.keras.models.load_model("invisor2.h5",custom_objects={'KerasLayer':hub.KerasLayer})
 
 @app.route('/')
 def hello_world():
@@ -35,12 +19,39 @@ def hello_world():
 
 @app.route("/get")
 def get_prediction():
-  # stock = request.args["stock"]
-  # newp=model.predict([text,num_s])
-  # y_new_inverse = scaler2.inverse_transform(newp)
-  # res=y_new_inverse[0][0]
+  stock = request.args["stock"]
 
-  return 'Prediction is '#.format(res)
+  with urllib.request.urlopen("https://invisor.axemgit2.repl.co/api/allNews?stock="+stock) as url:
+    Newsdata = json.loads(url.read().decode())
+  
+  picker = CherryPicker(Newsdata)
+  flat = picker.flatten().get()
+  textual_file = pd.DataFrame(flat)
+  textual_file = textual_file.iloc[:15]
 
+  with urllib.request.urlopen("https://invisor.axemgit2.repl.co/api/histData?stock="+stock) as url:
+    num_json = json.loads(url.read().decode())
+  num = pd.DataFrame(reversed(num_json))
 
-app.run(host='0.0.0.0', port=8080)
+  textual_file['combined'] = textual_file[textual_file.columns[1:27]].apply(lambda x: ','.join(x.dropna().astype(str)),axis=1)
+
+  textual_file['combined'].replace("[^a-zA-Z]"," ",regex=True, inplace=True)
+  textual_file['combined']=textual_file['combined'].apply(lambda x : x.lower())
+
+  textual_file=textual_file['combined']
+
+  # num=num['close']
+  # num = num.iloc[:15]
+
+  scaler2=MinMaxScaler(feature_range=(0,1))
+  num=num.values.reshape(-1,1)
+  num_s=scaler2.fit_transform(num)
+
+  newp=model.predict([textual_file,num_s])
+  y_new_inverse = scaler2.inverse_transform(newp)
+  res=y_new_inverse[0][0]
+  prediction = json.dumps(res.item())
+
+  return jsonify(prediction[:6])
+
+app.run()
